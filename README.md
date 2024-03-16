1e4a821ca2278e309d683ea09c85e16862583a72
Daryl <daryltanph@gmail.com>
2024-03-16 22:17:46 +0800

    Basic Functionality Achieved
    The script is able to get user input for a filename and use that user input to parse the appropriate HTML file and print the contents of the file.
    
    Print format is:
    <name> <rank>
    <name> <rank>
    
    where the names are sorted alphabetically and each new name is printed on a new line

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Google Baby Names Python Exercise
  to document my work on the Google Python Developer Class Exercise for Baby Names Web Page Parser
+
+This python coding exercise is taken from https://developers.google.com/edu/python/exercises/baby-names. Click the link to see the original full text of the exercise.
+
+## Problem Statement:
+We have many files that contain lists of the most popular baby names for boys and girls in a particular year.
+We would like to have a python script that can run in the command line that helps us parse and organize the data in these HTML files.
+
+## Functionality / Features
+### Basic Features
+The script must be able to read the files in a directory.
+The script will be able to organize the data into a dictionary.
+The script can print data to console.
+
+Suggested Milestones:
+1. Extract all the text from the file and print it
+2. Find and extract the year and print it
+3. Extract the names and rank numbers and print them
+4. Get the names data into a dict and print it
+5. Build the [year, 'name rank', ... ] list and print it
+6. Fix main() to use the ExtractNames list
+
+### Advanced Features
+The script should have some way to get user input through the command-line interface.
+The script should be able to take multiple files as input
+The script should be able to write to file.
+Implement a --summary flag feature when running the script that when provided together with filepaths, will read the files and generate a summary file
+
+
+
--- babynames.py
+++ babynames.py
@@ -9,60 +9,66 @@
 import sys
 import re
 
-"""Baby Names exercise
-
-Define the extract_names() function below and change main()
-to call it.
-
-For writing regex, it's nice to include a copy of the target
-text for inspiration.
-
-Here's what the html looks like in the baby.html files:
-...
-<h3 align="center">Popularity in 1990</h3>
-....
-<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
-<tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
-<tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
-...
-
-Suggested milestones for incremental development:
- -Extract the year and print it
- -Extract the names and rank numbers and just print them
- -Get the names data into a dict and print it
- -Build the [year, 'name rank', ... ] list and print it
- -Fix main() to use the extract_names list
-"""
-
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # +++your code here+++
-  return
+    """
+    Given a file name for baby.html, returns a list
+    starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    print the year at the top, then on each new line
+    print the names and rank in alphabetical order
+    """
+    with open(filename, 'r') as f:
+        file_text = f.read()
+    year_match = re.search(r'Popularity\sin\s(\d{4})', file_text)
+    names_match = re.findall(r"<td>(\d+)</td><td>(\w+)"
+                             r"</td><td>(\w+)</td>",
+                             file_text)
+    text_to_print = [year_match.group(1)]
+    ranked_name_dict = {}
+    for matches in names_match:
+        (name_rank, boy_name, girl_name) = matches
+        if boy_name not in ranked_name_dict:
+            ranked_name_dict[boy_name] = name_rank
+        if girl_name not in ranked_name_dict:
+            ranked_name_dict[girl_name] = name_rank
+    for name in sorted(ranked_name_dict):
+        text_to_print.append(name + ' ' + ranked_name_dict[name])
+    print("\n".join(text_to_print))
+    return
+
+def get_user_filename():
+    filename = input("Key in the filepath of the HTML file you wish to use: ")
+    return filename
+
+def _run_extract_names():
+    filename = get_user_filename()
+    extract_names(filename)
 
 
 def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
+    _run_extract_names()
 
-  if not args:
-    print('usage: [--summaryfile] file [file ...]')
-    sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    # This command-line parsing code is provided.
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    # args = sys.argv[1:]
+    #
+    # if not args:
+    #     print('usage: [--summaryfile] file [file ...]')
+    #     sys.exit(1)
 
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
+    # Notice the summary flag and remove it from args if it is present.
+    # summary = False
+    # if args[0] == '--summaryfile':
+    #     summary = True
+    #     del args[0]
+
+    # +++your code here+++
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
+
 
 if __name__ == '__main__':
-  main()
+    main()
